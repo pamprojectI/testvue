ac2b2b0991dd7520160b188a6d0131cb321efd2c
pamproject <pamproject@git.com>
2024-08-29 09:03:43 +0700

    test version

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<script setup>
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+// import MyNev from './components/MyNev.vue'
+import MyCard from './components/MyCard.vue'
+// import items from './test.json'
+import Menu from './components/Menu.vue'
+import list from './list.json'
+</script>
+
+<template>
+<div>
+  <header>
+    <Menu />
+    <!-- <MyNev /> -->
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="512" /> 
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div> -->
+  </header>
+
+  <main>
+    <h1>TheWelcome</h1>
+    <div className="grid-container">
+      <MyCard v-bind="list[0]" />
+      <MyCard v-bind="list[1]" />
+      <MyCard v-bind="list[2]" />
+      
+    </div>
+    <!-- <TheWelcome /> -->
+  </main>
+</div>
+</template>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-column-gap: 0.5rem;
+  grid-row-gap: 0.5rem;
+}
+@media (min-width: 576px) {
+  .grid-container { grid-template-columns: repeat(1, 1fr); }
+}
+@media (min-width: 992px) {
+  .grid-container { grid-template-columns: repeat(3, 1fr); }
+}
+
+/* header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+} */
+</style>
---
+++ src/components/MyCard.vue
@@ -0,0 +1,38 @@
+<template lang="">
+    <div class="card">
+        <!-- <img src="https://4kwallpapers.com/images/wallpapers/windows-11-dark-mode-blue-stock-official-1920x1080-5630.jpg" alt="Avatar" style="width:100%">
+            <div class="container">
+                <h4><b>John Doe</b></h4> 
+                <p>Architect & Engineer</p> 
+            </div> -->
+            <img :src="image" alt="profile" style="width:100%">
+            <div class="container">
+                <h2><b>{{ name }}</b></h2> 
+                <p>{{ tel }}</p> 
+            </div>    
+    </div>
+</template>
+
+<script>
+export default {
+  // props: ['id', 'name', 'nickname', 'tel', 'email', 'image']
+  props: ['id', 'name', 'tel', 'image']
+
+}
+</script>
+
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  /* width: 40%; */
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
